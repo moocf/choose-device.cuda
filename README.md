781f0b282bc628950a795ab21b6410ccba7ec7ef
Subhajit Sahu <wolfram77@gmail.com>
2020-09-12 15:38:05 +0530

    repo: readying for creating vs project

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# choose-device
 Selection of compute-device which best matches criteria.
+
+```c
+1. Check how many compute devices are attached.
+2. List some properties of each device.
+```
+
+```bash
+# OUTPUT
+COMPUTE DEVICE 0:
+Name: GeForce GTX 1050
+Compute capability: 6.1
+Multiprocessors: 5
+Clock rate: 1493 MHz
+Global memory: 4096 MB
+Constant memory: 64 KB
+Shared memory per block: 48 KB
+Registers per block: 65536
+Threads per block: 1024 (max)
+Threads per warp: 32
+Block dimension: 1024x1024x64 (max)
+Grid dimension: 2147483647x65535x65535 (max)
+Device copy overlap: yes
+Kernel execution timeout: yes
+```
+
+See [main.cu] for code.
+
+[main.cu]: main.cu
+
+
+### references
+
+- [CUDA by Example :: Jason Sanders, Edward Kandrot](http://www.mat.unimi.it/users/sansotte/cuda/CUDA_by_Example.pdf)
---
+++ main.cu
@@ -0,0 +1,34 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <stdio.h>
+#include "error.h"
+
+
+// 1. Check how many compute devices are attached.
+// 2. List some properties of each device.
+int main() {
+  int N;                         // 1
+  TRY( cudaGetDeviceCount(&N) ); // 1
+
+  cudaDeviceProp p;                        // 2
+  for (int i=0; i<N; i++) {                // 2
+    TRY( cudaGetDeviceProperties(&p, i) ); // 2
+    printf("COMPUTE DEVICE %d:\n", i);
+    printf("Name: %s\n", p.name);
+    printf("Compute capability: %d.%d\n", p.major, p.minor);
+    printf("Multiprocessors: %d\n", p.multiProcessorCount);
+    printf("Clock rate: %d MHz\n", p.clockRate / 1000);
+    printf("Global memory: %lld MB\n", p.totalGlobalMem / (1024*1024));
+    printf("Constant memory: %lld KB\n", p.totalConstMem / 1024);
+    printf("Shared memory per block: %lld KB\n", p.sharedMemPerBlock / 1024);
+    printf("Registers per block: %d\n", p.regsPerBlock);
+    printf("Threads per block: %d (max)\n", p.maxThreadsPerBlock);
+    printf("Threads per warp: %d\n", p.warpSize);
+    printf("Block dimension: %dx%dx%d (max)\n", p.maxThreadsDim[0], p.maxThreadsDim[1], p.maxThreadsDim[2]);
+    printf("Grid dimension: %dx%dx%d (max)\n", p.maxGridSize[0], p.maxGridSize[1], p.maxGridSize[2]);
+    printf("Device copy overlap: %s\n", p.deviceOverlap ? "yes" : "no");
+    printf("Kernel execution timeout: %s\n", p.kernelExecTimeoutEnabled ? "yes" : "no");
+    printf("\n");
+  }
+  return 0;
+}
